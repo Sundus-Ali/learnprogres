f8b7d25f8cf51005e97f3f965cc84e8d3debdd20
Sundus-Ali <Sundusalimahomed@gmail.com>
2026-02-07 06:13:56 +0300

    Implemented Node.js Backend with Auth APIs (Register/Login)

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+// Connect to Database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Simple Route for testing
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+// Routes
+app.use('/api/auth', require('./routes/authRoutes'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
