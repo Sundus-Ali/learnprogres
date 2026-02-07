490652726b5ae4f6d48f7d907fb3f824b9ea4cd4
Sundus-Ali <Sundusalimahomed@gmail.com>
2026-02-07 06:06:49 +0300

    Created Lesson Screen UI and connected to Dashboard

--- lib/views/dashboard/dashboard_screen.dart
+++ lib/views/dashboard/dashboard_screen.dart
@@ -66,8 +66,12 @@ class DashboardScreen extends StatelessWidget {
               ),
               trailing: const Icon(Icons.arrow_forward_ios, size: 16, color: Colors.grey),
               onTap: () {
-                // Navigate to Lesson Screen (Will be implemented next)
-                // Navigator.push...
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LessonScreen(title: course['title']),
+                  ),
+                );
               },
             ),
           );
